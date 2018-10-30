680076c88ec7c640bd64172e2739822701403e66
xi <chenxi410402@gmail.com>
2018-10-30 19:48:34 -0400

    add travis

--- test/userTest.py
+++ test/userTest.py
@@ -9,14 +9,14 @@ class UserFunctionTestCase(unittest.TestCase):
         db = UserHelper("test_user")
         assert db is not None
 
-    def testGetUser(self):
-        assert True
-
-    def testAddUser(self):
-        assert True
-
-    def testCheckPassword(self):
-        assert True
+    # def testGetUser(self):
+    #     assert True
+    #
+    # def testAddUser(self):
+    #     assert True
+    #
+    # def testCheckPassword(self):
+    #     assert True
 
 
 if __name__ == '__main__':
