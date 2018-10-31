695be450e03c4cdc71443273d79a7462e8e2ab4f
xi <chenxi410402@gmail.com>
2018-10-30 21:28:12 -0400

    add travis

--- test/testDB.py
+++ test/testDB.py
@@ -1,5 +1,8 @@
 import boto3
 from YourChef.credentials import region, aws_id, aws_key
+from test.SampleUser import users
+from passlib.hash import sha256_crypt
+
 
 dynamodb = boto3.resource('dynamodb', region_name=region, aws_access_key_id=aws_id, aws_secret_access_key=aws_key)
 
@@ -17,13 +20,17 @@ table = dynamodb.create_table(
 table.meta.client.get_waiter('table_exists').wait(TableName='test_user')
 print('created table {}.'.format('test_user'))
 
-# table = dynamodb.Table('testUser')
-# table.put_item(
-#     Item={
-#         'id': 2,
-#         'password': 'xxxxxx',
-#         'username': 'cindy',
-#         'email': 'xc@cu.edu',
-#     }
-# )
-# dynamodb.drop_table('testUser')
+
+for user in users:
+    response = table.put_item(
+        Item={
+            'email': user["email"],
+            'userid': user["userid"],
+            'password': sha256_crypt.encrypt(str(user["password"])),
+            'username': user["username"]
+        }
+    )
+    if response:
+        print(user['userid']+" saved")
+    else:
+        print(user['userid'] + " save fail")
--- test/userTest.py
+++ test/userTest.py
@@ -1,6 +1,8 @@
 import unittest
 
 from YourChef.dbHelper import UserHelper
+from passlib.hash import sha256_crypt
+from test.SampleUser import users
 
 
 class UserFunctionTestCase(unittest.TestCase):
@@ -9,13 +11,13 @@ class UserFunctionTestCase(unittest.TestCase):
         db = UserHelper("test_user")
         assert db is not None
 
-    # def testGetUser(self):
-    #     assert True
+    def testGetUser(self):
+        db = UserHelper("test_user")
+        for user in users:
+            user_data = db.get_user(user["userid"])
+            assert sha256_crypt.verify(user['password'], user_data['password']) and user_data['email'] == user['email'] and user_data['username'] == user['username']
     #
     # def testAddUser(self):
-    #     assert True
-    #
-    # def testCheckPassword(self):
     #     assert True
 
 
