5dd550c0da8a8b0ac855ebb1af6b06e7136ae336
xi <chenxi410402@gmail.com>
2018-11-07 16:04:46 -0500

    Unit test

--- YourChef/dbHelper.py
+++ YourChef/dbHelper.py
@@ -33,6 +33,7 @@ class UserHelper:
         region = os.environ.get('region')
         aws_id = os.environ.get('Access_key_ID')
         aws_key = os.environ.get('Secret_access_key')
+        print(region, aws_id, aws_key)
         dynamodb = boto3.resource('dynamodb', region_name=region, aws_access_key_id=aws_id, aws_secret_access_key=aws_key)
         self.table = dynamodb.Table(self.table_name_register)
         # self.response = self.table.scan()
