293d7bb9c3317f7587a0e6c692c2e9fb1c83678c
xi <chenxi410402@gmail.com>
2018-11-07 16:02:19 -0500

    Unit test

--- Test/TestUser.py
+++ Test/TestUser.py
@@ -1,8 +1,8 @@
 import unittest
 
 from YourChef.dbHelper import UserHelper
-from passlib.hash import sha256_crypt
-from Test.SampleUser import users
+# from passlib.hash import sha256_crypt
+# from Test.SampleUser import users
 
 
 class TestUserFunctionCase(unittest.TestCase):
@@ -11,11 +11,11 @@ class TestUserFunctionCase(unittest.TestCase):
         db = UserHelper("test_user")
         assert db is not None
 
-    def testGetUser(self):
-        db = UserHelper("test_user")
-        for user in users:
-            user_data = db.get_user(user["userid"])
-            assert sha256_crypt.verify(user['password'], user_data['password']) and user_data['email'] == user['email'] and user_data['username'] == user['username']
+    # def testGetUser(self):
+    #     db = UserHelper("test_user")
+    #     for user in users:
+    #         user_data = db.get_user(user["userid"])
+    #         assert sha256_crypt.verify(user['password'], user_data['password']) and user_data['email'] == user['email'] and user_data['username'] == user['username']
 
 
 if __name__ == '__main__':
