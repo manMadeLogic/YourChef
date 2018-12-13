c9fae2f9c70b12a2bbc4bebef8b358a0ead21c9b
Xingchen Zhou (David) <xingchenzhou1992@gmail.com>
2018-12-13 16:51:20 -0500

    Add files via upload

---
+++ Test/SampleResProfile.py
@@ -0,0 +1,36 @@
+# Test restaurant profiles
+users = [
+    {
+        'userid': "res1@bu.edu",
+        'username': "xcz",
+        'salt': 0.2,
+        'sour': 0.7,
+        "sweet": 0.2,
+        "spicy": 0.1
+    },
+
+    {
+        'userid': "res2@cu.edu",
+        'username': "xcz",
+        'salt': 0.3,
+        'sour': 0.5,
+        "sweet": 0.3,
+        "spicy": 1
+    },
+
+]
+
+insert_users = [
+
+    {
+        'userid': "res3@pu.edu",
+        'username': "xcz",
+        'salt': 0.2,
+        'sour': 0.5,
+        "sweet": 0.2,
+        "spicy": 0.1
+    }
+
+]
+
+
---
+++ Test/SampleUserProfile.py
@@ -0,0 +1,33 @@
+# Test user profiles
+users = [
+    {
+        'userid': "user1@bu.edu",
+        'salt': 0.3,
+        'sour': 0.7,
+        "sweet": 0.2,
+        "spicy": 0.1
+    },
+
+    {
+        'userid': "user2@cu.edu",
+        'salt': 0.3,
+        'sour': 0.5,
+        "sweet": 0.3,
+        "spicy": 1
+    },
+
+]
+
+insert_users = [
+
+    {
+        'userid': "user3@pu.edu",
+        'salt': 0.2,
+        'sour': 0.5,
+        "sweet": 0.2,
+        "spicy": 0.1
+    }
+
+]
+
+
