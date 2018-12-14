0fd5e212c6a6e1ec68a0fa2d57974773919500e4
Michelle <mstianll@yahoo.com>
2018-12-14 00:09:27 -0500

    Order and restaurant profile unit test add.

---
+++ Test/TestOrder.py
@@ -0,0 +1,21 @@
+import unittest
+from YourChef.orderHelper import OrderHelper
+from Test.SampletestOrder import orders, orders_failed
+
+
+class TestManageDishCase(unittest.TestCase):
+    def testServer(self):
+        server = OrderHelper('Order')
+        assert server is not None
+
+    def testRegister(self):
+        server = OrderHelper("Order")
+        for order in orders:
+            result, message = server.add_order(order['restaurant'], order['dishes'], order['total'], order['userid'])
+            assert result
+
+    def testFailRegister(self):
+        server = OrderHelper("Order")
+        for order in orders_failed:
+            result, message = server.add_order(order['restaurant'], order['dishes'], order['total'], order['userid'])
+            assert not result
